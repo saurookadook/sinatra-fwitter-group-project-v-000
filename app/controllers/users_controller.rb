83875e3b09f175dbdc55965e6ab5b7e54d91cfe0
Andy Maskiell <maskiella@gmail.com>
2018-02-23 07:10:43 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,6 @@ class UsersController < ApplicationController
 
   use Rack::Flash
 
-  # get '/signup' do
-  #
-  # end
-  #
-  # get '/login' do
-  #
-  # end
-
   get '/users/:slug' do
     @current_user = User.find_by_slug(params[:slug])
     erb :"users/show"
